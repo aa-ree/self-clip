6f35d43940930cc1d64ff6e605bebf3dde4a7c60
reehoiyan <re.hoiyan@gmail.com>
2016-09-21 12:39:36 +0800

    add social share button

--- assets/main.js
+++ assets/main.js
@@ -1,6 +1,14 @@
+(function(d, s, id) {
+      var js, fjs = d.getElementsByTagName(s)[0];
+      if (d.getElementById(id)) return;
+      js = d.createElement(s); js.id = id;
+      js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.7";
+      fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
+
 function checkImportAtt(){
     return ('import' in document.querySelector('link[rel="import"]'));
 }
-      if(!checkImportAtt()){
-        alert("Sorry, your browser not supporting import attribute without polyfill.");
-      }
+if(!checkImportAtt()){
+  alert("Sorry, your browser not supporting import attribute without polyfill.");
+}
